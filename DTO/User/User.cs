6e7904e1ca5e020e3f7603352b70bfe677cb9fd0
Blessings Chidambe <bchidambe@gmail.com>
2024-02-15 17:39:14 +0200

    ADD: DTO & Service Sample

---
+++ DTO/User/User.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MAUI.DTO.User;
+
+public partial class User : ObservableObject
+{
+    [ObservableProperty]
+    string? _userId;
+
+    [ObservableProperty]
+    string? _firstName;
+
+    [ObservableProperty]
+    string? _lastName;
+}
---
+++ Services/UserService.cs
@@ -0,0 +1,41 @@
+﻿using MAUI.DTO.User;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MAUI.Services;
+
+
+public interface IUserService
+{
+    Task<User> GetUserById(string UserId);
+    Task<List<User>> GetAllUsers();
+
+}
+
+internal class UserService : IUserService
+{
+    readonly IApiRequestHelper _apiRequestHelper;
+    public UserService(IApiRequestHelper apiRequestHelper)
+    {
+        _apiRequestHelper = apiRequestHelper;
+    }
+
+    public async Task<User> GetUserById(string UserId)
+    {
+        var result = await _apiRequestHelper.SendAsync<User>(HttpMethod.Post, ApiNames.CentralAPI, "/users", UserId);
+
+        return result ?? new();
+    }
+
+    public async Task<List<User>> GetAllUsers()
+    {
+        var result = await _apiRequestHelper.GetAsync<List<User>>(ApiNames.CentralAPI, "/users");
+
+        return result ?? new();
+    }
+
+    
+}
