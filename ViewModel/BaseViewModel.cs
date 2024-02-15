58df2e973216621c2fc0776b745f08a5954f80c9
Blessings Chidambe <bchidambe@gmail.com>
2024-02-15 19:12:25 +0200

    ADD: BaseViewModel

---
+++ ViewModel/BaseViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MAUI.ViewModel;
+
+public partial class BaseViewModel : ObservableObject
+{
+    public BaseViewModel() { }
+
+    [ObservableProperty]
+    [NotifyPropertyChangedFor(nameof(IsNotBusy))]
+    bool _isBusy;
+
+    public bool IsNotBusy => !IsBusy;
+
+    public virtual Task InitAsync()
+    {
+        return Task.CompletedTask;
+    }
+}
